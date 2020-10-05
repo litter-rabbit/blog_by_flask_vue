35055df5a24952aec10b642cb9ae2394e2e82ef3
litter-rabbit <709343607@qq.com>
2020-10-05 22:54:20 +0800

    2.Vue 通过axios访问flask的api

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -2,13 +2,15 @@
 from flask import Flask
 
 from config import config
-
+from flask_cors import CORS
 
 def create_app(configname=None):
 
     app=Flask(__name__)
     app.config.from_object(config[configname])
+    CORS(app)
     register_blueprints(app)
+
     return app
 
 
